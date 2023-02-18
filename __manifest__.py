c1b3572687aea2314f883b2d43d38e089366fb2e
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2023-02-18 11:25:23 -0500

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,15 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería, S.L.
+# Copyright (C) 2015 Antiun Ingeniería, S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Suggest to create user account when buying",
     "summary": "Suggest users to create an account when buying in the website",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.1",
     "category": "Website",
-    "website": "https://www.tecnativa.com",
-    "author": "Tecnativa, "
-              "LasLabs, "
-              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Tecnativa, " "LasLabs, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": [
-        "website_sale",
-    ],
-    "data": [
-        "views/website_sale.xml",
-    ],
+    "depends": ["website_sale"],
+    "data": ["views/website_sale.xml", "views/assets.xml"],
+    "post_init_hook": "post_init_hook",
 }
